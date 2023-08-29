01aafa4fbd74c391df0e5295e63b2dc40fdd34db
Nandini Nema <nandininema05@gmail.com>
2023-08-29 15:39:30 -0600

    Adding new files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # csci1300
+
+Code for class CSCI 1300 at CU Boulder
---
+++ hmwk0/hello1300v2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // print statement   
+    cout << "Hello World! Hello CSSI 1300" << endl; 
+    return 0;
+}
