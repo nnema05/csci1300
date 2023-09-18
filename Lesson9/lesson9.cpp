d107a92946fcf15961950aa91196b7eb617b0270
Nandini Nema <nandininema05@gmail.com>
2023-09-18 13:11:19 -0600

    fixing lesson 9

--- Lesson9/lesson9.cpp
+++ Lesson9/lesson9.cpp
@@ -34,8 +34,6 @@ int main() {
     // local variable: only seen in the fucntion, or code block, where it is defined, 
         // or passed as an arguement 
     scopeTest(45, 12);
-    int c = 45;
-    int d = 12;
     int c = 45; // local to main
     int d = 12; // local to main
     scopeTest(c, d); // this will pass in the value stored in c, not the vairable
@@ -46,5 +44,34 @@ int main() {
     }
     // even if conditional is trye and variable is created, it is destroyed after conditional exits
     // cout << i << endl; // this does not work because variable not in scope outside of conditional 
+
+
+    /* STRINGS */
+    // Every character you type has an ASCII number associated with it 
+    // Ex. A is a 65
+
+    // a String is a sequence of individual alpha-numeric characters
+    string strA; // string variable
+    // Can do a lot of manipulation with a string  
+        // if you strA., lots of methods come up of things you can do to a string
+        // all are part of the string class (a class is a container of stuff)
+    strA = "this is a string";
+    // can get length of string
+    int strALength = strA.length(); // call the length function of our string 
+    cout << strALength << endl;
+    strA = "Will the Rockies win the world series?";
+    // get substring of string
+    string str2 = strA.substr(9, 7);
+        // will start at position 9 and print the 7 characters
+    cout << str2 << endl; // prints "Rockies"
+
+    // user input of string 
+    cout << "type two words" << endl;
+    // cin >> str2;
+    // cout << str2 << endl; // this only stores the first word because cin stops after white space!
+    // to fix this use function called getline
+    getline(cin, str2); // reads the whole line plus spaces, not just first word
+    cout << str2 << endl;
+
 
 }
